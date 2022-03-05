9440a000c8821018dc52c6283c56c5f994015cae
Jennifer <jennifersusan@Jennifers-MBP.cable.rcn.com>
2022-03-05 07:33:16 -0600

    added basic info to restrooms page

--- src/pages/Restrooms.js
+++ src/pages/Restrooms.js
@@ -1,6 +1,17 @@
-const Restrooms = () => {
+const Restrooms = ({restrooms}) => {
     return(
-        <h1>Restrooms</h1>
+        <>
+            <h1>Restrooms</h1>
+            {restrooms.map((restroom) => (
+            <div key={restroom.id}>
+                <h3>{restroom.name}</h3>
+                <p>{restroom.street} {restroom.city}, {restroom.state}</p>
+                <p>Is it unisex: {restroom.unisex}</p>
+                <p>Comments: {restroom.comment}</p>
+            </div>
+        
+    ))}
+     </>
     )
 };
 
