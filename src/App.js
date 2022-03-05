cbdf011f0e42a64cd861d15cd150870e8d44b46b
Jennifer <jennifersusan@Jennifers-MBP.cable.rcn.com>
2022-03-05 13:01:03 -0600

    basic navbar styling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import {Routes, Route} from "react-router-dom";
 import { useState, useEffect } from "react";
 import Home from "./pages/Home"; 
 import Caregivers from "./pages/Caregivers";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import Categories from "./pages/Categories/Categories";
 import Mobility from "./pages/Mobility/Mobility";
+import About from "./pages/About/About";
 const axios = require('axios');
 
 function App() {
@@ -26,6 +27,8 @@ function App() {
       <Route path='/categories' element={<Categories />}/>
       <Route path='/mobility' element={<Mobility handleShowIframe={handleShowIframe} iframeVisible={iframeVisible}/>}/>
       <Route path='/caregivers' element={<Caregivers />}/>
+      <Route path='/about' element={<About />}>
+      </Route>
     </Routes>
     </>
   );
---
+++ src/pages/About/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return(
+        <>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, velit quisquam incidunt eveniet fuga optio voluptatibus nihil a totam itaque voluptate tempore vitae molestias adipisci explicabo suscipit minima officia odit.</p>
+        </>
+    )
+}
+
+export default About;
