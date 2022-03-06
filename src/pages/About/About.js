466a4d27dba3e2ddea355304a87b73ca7c320bda
Jennifer <jennifersusan@Jennifers-MBP.cable.rcn.com>
2022-03-05 21:44:42 -0600

    removed old about info

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,9 +3,9 @@ import {Wrapper, Content} from './About-styles';
 const About = () => {
     return(
         <Wrapper>
-            <h1>About</h1>
+            
             <Content>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, velit quisquam incidunt eveniet fuga optio voluptatibus nihil a totam itaque voluptate tempore vitae molestias adipisci explicabo suscipit minima officia odit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, velit quisquam incidunt eveniet fuga optio voluptatibus nihil a totam itaque voluptate tempore vitae molestias adipisci explicabo suscipit minima officia odit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, velit quisquam incidunt eveniet fuga optio voluptatibus nihil a totam itaque voluptate tempore vitae molestias adipisci explicabo suscipit minima officia odit. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, velit quisquam incidunt eveniet fuga optio voluptatibus nihil a totam itaque voluptate tempore vitae molestias adipisci explicabo suscipit minima officia odit.</p>
+           
             </Content>
         </Wrapper>
     )
