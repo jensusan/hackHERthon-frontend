f5799b4b1c17fc76a5a4c21731bcc3402ee19d26
Jennifer <jennifersusan@Jennifers-MBP.cable.rcn.com>
2022-03-05 23:34:22 -0600

    added styling to categories

--- src/pages/Categories/Categories-styles.js
+++ src/pages/Categories/Categories-styles.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    margin: 10px;
+    margin: 40px;
+
+    h2 {
+        font-size: 36;
+    }
 `;
 
 export const Content = styled.div`
@@ -10,15 +14,18 @@ export const Content = styled.div`
     flex-wrap: wrap;
 
     .box {
-        background: #D3D3D3;
-        width: 150px;
-        height: 150px;
+        background: #034F96;
+        color: white;
+        font-size: 22px;
+        width: 236px;
+        height: 236px;
         padding: 10px;
-        margin: 40px;
+        margin: 50px;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        border-radius: 5%;
     }
 
 
