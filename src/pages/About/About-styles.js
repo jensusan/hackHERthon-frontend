eb5ec67b20db7a291ba9be5818a7aa37f686255c
Jennifer <jennifersusan@Jennifers-MBP.cable.rcn.com>
2022-03-06 04:21:59 -0600

    styled about page

--- src/pages/About/About-styles.js
+++ src/pages/About/About-styles.js
@@ -1,13 +1,50 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-    text-align: center;
-    h1 {
-        letter-spacing: 1px;
-    }
+   margin: 0px 50px;
     
 `;
 
 export const Content = styled.div`
-    margin: 50px;
+h2 {
+    text-align: center;
+    color: #002E5B;
+    font-size: 36px;
+    margin: 10px 300px;
+}
+
+.boxes {
+    display: flex;
+    gap: 40px;
+    color: white;
+    justify-content: center;
+    
+   
+}
+
+.boxes p {
+    background-color: #002E5B;
+    border-radius: 10px;
+    padding: 25px 30px;
+    font-size: 26px;
+    text-align: center;
+    
+}
+
+.content-container {
+    display: flex;
+    gap: 60px;
+    font-size: 20px;
+}
+
+.paragraphs {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 50vw;
+}
+
+.list-content p {
+    margin-bottom: 10px;
+}
 `;
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,9 +3,31 @@ import {Wrapper, Content} from './About-styles';
 const About = () => {
     return(
         <Wrapper>
-            
             <Content>
-           
+                <h2>Safety shouldn’t come with barriers.</h2>
+                <h2>That’s why we’re here to help remove them.</h2>
+                <div className="boxes">
+                    <p>One billion people,
+15% of the world's population, experience some form of disability.</p>
+<p>One billion people,
+15% of the world's population, experience some form of disability.</p>
+<p>Some people are have difficulty accessing essential resources because of their disability.</p>
+                    </div>
+                 <h2>How do we do this?</h2>   
+                 <div className="content-container"><div className="paragraphs">
+                     <p>Evac-Ability aims to knock down the barriers that block people with disabilities from accessing their right to safety. </p><p>
+
+We spoke to real people - from the mother of a non-speaking autistic child, to a young adult with sensory and mobility issues. They wanted a way to access relevant, reliable safety information during times of crisis. </p></div>
+<div className="list-content">
+    <p>We connect people with disabilities to the information they need, right where they are:</p>
+    <ul>
+        <li>Nearby emergency shelters</li>
+        <li>  Accessible restroom locations</li>
+        <li>Food pantries/distribution</li>
+        <li>Delivery of medicines, medical equipment, vitamins, and hygiene products</li>
+    </ul>
+    </div>
+                     </div>
             </Content>
         </Wrapper>
     )
